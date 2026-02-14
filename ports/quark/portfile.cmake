e5b79ed635800af2fcaf1e9316028eb7f9f90495
pnnh <linyangz@outlook.com>
2026-02-14 15:38:56 +0800

    test

--- ports/quark/portfile.cmake
+++ ports/quark/portfile.cmake
@@ -2,14 +2,12 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO pnnh/quark
     REF v0.1.0
-    SHA512 280c1b3ac89ad7ed34e4d6910a2d01b14e4e0d8c7bd30e4fb4de804c90f87e234232f4c25bf991c9160e4160f8d4eb03125260c5e0b44efcc8c0e75233fff372
+    SHA512 51c1f4f31cda9de7355f297d5f75a2773dcac56a3630dc7a48bc6d37c0761a06cd2e6714d9baf60547271ec6e8840a15ee2eb4d81e74e6877d5027c39c6fa602
     HEAD_REF main
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS
-        -DBUILD_TESTING=OFF
 )
 
 vcpkg_cmake_install()
